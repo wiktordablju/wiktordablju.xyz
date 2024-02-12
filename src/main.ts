932030216df6580d4caeafdd77d4f1333311ce0d
dablju <wiktordablju@gmail.com>
2024-02-12 21:18:06 +0100

    added Vercel Speed Insights

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,10 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { inject } from '@vercel/analytics';
+import { injectSpeedInsights } from '@vercel/speed-insights';
 
 inject();
+injectSpeedInsights();
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)
